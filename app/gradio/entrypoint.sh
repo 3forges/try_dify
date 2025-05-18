7946103775b24bc6ae4adf5d6f9c1cf4984ea132
Jean-Baptiste-Lasselle <jean.baptiste.lasselle.pokus@gmail.com>
2025-05-18 15:57:53 +0200

    containerization of whisper ai

--- app/gradio/entrypoint.sh
+++ app/gradio/entrypoint.sh
@@ -10,18 +10,23 @@ export PATH="$PATH:$HOME/anaconda3/bin"
 
 
 # Enable strict mode.
-set -euo pipefail
+# set -euo pipefail
 
 conda --version
 # ... Run whatever commands ...
 
 # Temporarily disable strict mode and activate conda:
-set +euo pipefail
-conda activate tolt_env
+# set +euo pipefail
+# conda init
+# conda activate tolt_env
 
 # Re-enable strict mode:
-set -euo pipefail
+# set -euo pipefail
 
 
-python VoyageVocab.py
+# conda run -n tolt_env /bin/bash -c 'pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu && \
+#   pip install git+https://github.com/openai/whisper.git && \
+#   python VoyageVocab.py'
+
+conda run -n tolt_env /bin/bash -c 'python VoyageVocab.py'
 # python VoyageVocab.py
--- app/gradio/install.py.dependencies.sh
+++ app/gradio/install.py.dependencies.sh
@@ -4,6 +4,8 @@
 # That's for CPU, not CUDA, not ROCm
 pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
 
-pip install openai-whisper
+# pip install openai-whisper
+pip install git+https://github.com/openai/whisper.git
+
 
 # pip install gradio langchain langchain-community
