418fbedb70cd50a4e848fcb90f8a8330e06d23e2
Jean-Baptiste-Lasselle <jean.baptiste.lasselle.pokus@gmail.com>
2025-05-18 15:28:48 +0200

    containerization of whisper ai

--- app/gradio/entrypoint.sh
+++ app/gradio/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
+source ~/.bashrc
+
+export PATH="$PATH:$HOME/anaconda3/bin"
 
 conda --version
 conda run --no-capture-output -n tolt_env python VoyageVocab.py
