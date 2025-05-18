9f1603325c6ea55c4b798be5787bf40c5b3ef16b
Jean-Baptiste-Lasselle <jean.baptiste.lasselle.pokus@gmail.com>
2025-05-18 15:32:06 +0200

    containerization of whisper ai

--- app/gradio/entrypoint.sh
+++ app/gradio/entrypoint.sh
@@ -1,10 +1,22 @@
-#!/bin/bash
+#!/bin/bash --login
+# The --login ensures the bash configuration is loaded,
+# enabling Conda.
 
 # pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
-source ~/.bashrc
 
-export PATH="$PATH:$HOME/anaconda3/bin"
+# Enable strict mode.
+set -euo pipefail
 
 conda --version
-conda run --no-capture-output -n tolt_env python VoyageVocab.py
+# ... Run whatever commands ...
+
+# Temporarily disable strict mode and activate conda:
+set +euo pipefail
+conda activate tolt_env
+
+# Re-enable strict mode:
+set -euo pipefail
+
+
+python VoyageVocab.py
 # python VoyageVocab.py
