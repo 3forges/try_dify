ea07d5c07ad579de9ff6cbb5e5013672b00c3cfe
Jean-Baptiste-Lasselle <jean.baptiste.lasselle.pokus@gmail.com>
2025-05-18 18:04:30 +0200

    containerize whisper

--- app/gradio/entrypoint.2.sh
+++ app/gradio/entrypoint.2.sh
@@ -29,5 +29,6 @@ conda --version
 #   python VoyageVocab.py'
 # conda run -n tolt_env /bin/bash -c '/tolt/app/install.py.dependencies.sh'
 # conda run -n tolt_env /bin/bash -c 'python VoyageVocab.py'
+conda activate tolt_env
 python VoyageVocab.py
 # python VoyageVocab.py
